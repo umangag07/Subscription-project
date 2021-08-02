2634f5b76b291f9e0f0161c65126cb8110eccdfd
umang <25umangagarwal@gmail.com>
2021-08-02 12:25:04 +0530

    chore:changed heading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,14 +38,8 @@ function App() {
       <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <img
-              className="mx-auto h-12 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-              alt="Workflow"
-            />
             <h2 className="mt-6 text-center text-3xl  text-gray-900">
-              Subscribe to our mailing list for more personalised tips for
-              development.
+              Subscribe to our mailing list for more personalised content.
             </h2>
           </div>
 
